b7f42b3f8cd888c408c8d83571506cbabde8e0c2
ActonGames <actongames@ud.wiki>
2024-11-02 19:54:57 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    clickableImage: 'https://imgs.search.brave.com/nm-f1yjacy1eNLuTq1JT6Re9Jf0zy2DFlevLo4FqoaU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA2LzgxLzA2LzAz/LzM2MF9GXzY4MTA2/MDMwOF9WMENDZW0w/U0hHVWc0TE9iTjhJ/SHFzYWhLb0hhVXMy/ei5qcGc',
+    clickSound: 'clickSound.mp3',
+    upgradeSound: 'upgradeSound.mp3',
+    backgroundMusic: 'backgroundMusic.mp3',
+};
